6da3b938772095fb5020b74677742f5a1f5aad4f
TheCheesyWiggle <fjlvanmontfort@gmail.com>
2025-08-12 07:19:36 +0100

    testing linter

--- src/app/Bauhaus/Bauhaus.js
+++ src/app/Bauhaus/Bauhaus.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function BauhausPage() {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen bg-[#f5ecd7] text-black p-4">
-      <h1 className="text-5xl font-bold">Hello World</h1>
+      <h1 className="text-5xl font-bold">Hello World!</h1>
     </main>
   );
 }

4d78d1ac167038740b461da8cbf6dc63569db4b2
TheCheesyWiggle <fjlvanmontfort@gmail.com>
2025-09-23 17:20:10 +0100

    build issues fixed

---
+++ src/app/explorer/page.js
@@ -0,0 +1,11 @@
+import "../../app/globals.css";
+import { NavBar } from "../../components/navigation/NavBar.js";
+import TravelGlobe from "../../components/gallery/TravelGlobe.js";
+
+export default function ExplorerPage() {
+  return (
+    <NavBar>
+      <TravelGlobe />
+    </NavBar>
+  );
+}
---
+++ src/app/terminal/page.js
@@ -0,0 +1,11 @@
+import "../../app/globals.css";
+import { NavBar } from "../../components/navigation/NavBar.js";
+import Terminal from "../../app/Terminal.js";
+
+export default function TerminalPage() {
+  return (
+    <NavBar>
+      <Terminal />
+    </NavBar>
+  );
+}
